a9cd9de6d69502c59bccbb414beea74b0ca87d6a
Ahmad Danang <ahmad.danang91@gmail.com>
2021-01-27 11:23:39 +0700

    watch state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducers/rootReducer";
 import rootSaga from "./middleware/sagas";
@@ -7,7 +7,16 @@ import rootSaga from "./middleware/sagas";
 const sagaMiddleware = createSagaMiddleware();
 
 //store
-const globalStore = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const globalStore = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
+  // applyMiddleware(sagaMiddleware)
+);
 
 //run saga
 sagaMiddleware.run(rootSaga);
